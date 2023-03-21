ff4b19d730999d0ba4b840b8315f4690b6470e0c
AX200M <54910564+AX200M@users.noreply.github.com>
2023-03-21 12:31:27 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import main
+import requests
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+IP = "65.21.130.125"
+Port = "8081"
+
+payload = {'remoteAddress': IP, 'portNumber': Port}
+r = requests.post('https://ports.yougetsignal.com/check-port.php', params=payload)
+#print(r.text)
+
+if (r.text == '<p><img src="/img/flag_red.gif" alt="Closed" style="height: 1em; width: 1em;" /> Port <a href="http://en.wikipedia.org/wiki/Port_8081" target="_blank" />8081</a> is closed on 65.21.130.125.</p>'):
+    print("Not Open")
+
+else:
+    print("Port Open")
+
+@app.get("/")
+def read_root():
+    return {r.text}
