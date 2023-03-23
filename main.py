4908e7ba58fb75bba43d60f57572d6095c5838bf
AX200M <54910564+AX200M@users.noreply.github.com>
2023-03-23 16:44:06 +0000

    Added discord emoji to response

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ def read_root(remoteAddress: str, portNumber: int):
     payload = {'remoteAddress': remoteAddress, 'portNumber': portNumber}
     r = requests.post('https://ports.yougetsignal.com/check-port.php', params=payload)
     if "<p><img src=\"/img/flag_red.gif\" alt=\"Closed\" style=\"height: 1em; width: 1em;\" />" in r.text:
-        return {"message": "Server Is Offline"}
+        return {"message": "Server Is Offline :red_circle:"}
     else:
-        return {"message": "Server Is Online"}
+        return {"message": "Server Is Online :green_circle:"}
